5bda1315978e97810fb54a25215ac2e9127ed7e1
yugoung33 <yugoung33@HYJ>
2016-01-06 17:15:27 +0900

    .

--- sample01/src/main/java/java76/pms/Hello.java
+++ sample01/src/main/java/java76/pms/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("hello....");
+    hwang();
 
   }
+  
+private static void hwang () {
+  System.out.println("감자탕 빨리 먹구싶오요");
+}
 
 }

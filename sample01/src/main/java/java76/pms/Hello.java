2c1e735610202ebd44be1f533736cebbf81444d4
Jinyoung <jinyoung.eom@gmail.com>
2016-01-06 17:14:50 +0900

    ohora

--- sample01/src/main/java/java76/pms/Hello.java
+++ sample01/src/main/java/java76/pms/Hello.java
@@ -4,7 +4,11 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("hello....");
-
+    ohora();
+  }
+  
+  private static void ohora() {
+    System.out.println("하하핳");
   }
 
 }

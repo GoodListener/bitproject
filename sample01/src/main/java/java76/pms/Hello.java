aac8d04681bacfd27c7e9169c4e2f4b04a1e032f
E KIM <zigzaggg07@gmail.com>
2016-01-06 17:57:16 +0900

    커밋먼저

--- sample01/src/main/java/java76/pms/Hello.java
+++ sample01/src/main/java/java76/pms/Hello.java
@@ -5,11 +5,15 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("hello....");
     ohora();
+    kimkim();
   }
   
   private static void ohora() {
     System.out.println("하하핳111");
   }
   
+  public static void kimkim() {
+    System.out.println("어어");
+  }
 
 }
